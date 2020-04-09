8c7e10ee56dc7600acfd0e84497c64fd42896938
Devin Riegle <devin.riegle@joinroot.com>
2020-04-09 19:22:33 -0400

    Add JSON helper

--- server/spec/requests/auth_request_spec.rb
+++ server/spec/requests/auth_request_spec.rb
@@ -3,20 +3,14 @@ RSpec.describe "Authentication", type: :request do
     let(:user_params) { attributes_for(:user) }
 
     context "when given the required attributes" do
-      it "creates a user" do
+      it "creates a user and auth token" do
         expect do
           post "/api/v1/auth/register", :params => { :user => user_params }
 
           expect(response).to have_http_status(:created)
-        end.to change(User, :count).by(1)
-      end
-
-      it "creates an auth token" do
-        expect do
-          post "/api/v1/auth/register", :params => { :user => user_params }
-
-          expect(response).to have_http_status(:created)
-        end.to change(AuthToken, :count).by(1)
+          expect(json_body[:resource][:id]).not_to eq(nil)
+          expect(json_body[:resource][:email]).to eq(user_params[:email])
+        end.to change(User, :count).by(1).and change(AuthToken, :count).by(1)
       end
 
       context "when they are invalid" do
