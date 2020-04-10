5d718e55e266be42b62db2988a385da86119159f
Devin Riegle <devin.riegle@joinroot.com>
2020-04-10 11:40:28 -0400

    remove sprocket railtie

--- server/config/application.rb
+++ server/config/application.rb
@@ -12,7 +12,7 @@ require "action_mailer/railtie"
 # require "action_text/engine"
 # require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 require "dotenv-rails" unless Rails.env.production?
 
