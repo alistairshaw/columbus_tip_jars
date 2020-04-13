2068cae6a3030b9f2fa841b4116137baff1d6c97
Devin Riegle <devin.riegle@joinroot.com>
2020-04-13 13:59:02 -0400

    Add about page

---
+++ web/src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box, Container, Typography } from '@material-ui/core'
+
+export default function AboutPage() {
+  return (
+    <Container>
+      <Typography color={'textPrimary'} variant={'h2'}>{'About Looking Good Columbus'}</Typography>
+
+      <Box marginTop={5}>
+        <Typography variant={'h6'}>
+          We exist to provide ways for cosmetic industry workers, affected by COVID-19, to receive financial support during these tough times.
+        </Typography>
+      </Box>
+
+    </Container>
+  )
+}
