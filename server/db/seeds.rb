15e488fd8f2ffd64b3430752282312a0f575d106
Katie Macke <katie.macke@joinroot.com>
2020-04-13 11:25:48 -0400

    Add some database seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+25.times do
+  FactoryBot.create(:user_profile, user: FactoryBot.create(:user, password: "test")).tap do |profile|
+    rand(0..5).times do
+      FactoryBot.create(:donation_method, profile: profile)
+    end
+  end
+end
+
+15.times do
+  FactoryBot.create(:business_profile, user: FactoryBot.create(:user, password: "test"))
+end
