9c1bcd39ee80ba69782dee3005ad549587318799
Devin Riegle <devin.riegle@joinroot.com>
2020-04-13 10:04:10 -0400

    Fix Next ENV variables

--- web/next.config.js
+++ web/next.config.js
@@ -1,11 +1,12 @@
 const path = require('path')
+const withNextEnv = require('next-env')()
 
 require('dotenv-load')()
 
-module.exports = {
+module.exports = withNextEnv({
   webpack: (config) => {
     config.resolve.alias['src'] = path.join(__dirname, 'src')
 
     return config
   },
-}
+})
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -4,6 +4,10 @@ import useAuth from 'src/hooks/use-auth'
 export default function IndexPage() {
   const { profile } = useAuth()
 
+  // TODO: Remove after testing on domain
+  // eslint-disable-next-line
+  console.log({ apiUrl: process.env.NEXT_PUBLIC_API_URL })
+
   return (
     <div>
       <h1>Welcome to the Columbus Tip Jars {profile?.email}</h1>
