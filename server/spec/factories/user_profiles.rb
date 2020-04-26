98764c02b6cb0f8a2fd4ab7cb97203ac4ed7c6e1
Alistair Shaw <alistair@loopreturns.com>
2020-04-26 12:01:15 -0400

    Update factory bot to remove photo_url for seeding

--- server/spec/factories/user_profiles.rb
+++ server/spec/factories/user_profiles.rb
@@ -26,7 +26,6 @@
 FactoryBot.define do
   factory :user_profile do
     user_name { "FakeUserName1" }
-    photo_url { "www.my-fake-photo.com" }
     industry { "Fake Industry" }
     nickname { "Fake Nickname" }
     user
