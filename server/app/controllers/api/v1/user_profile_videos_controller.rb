a829e78717cbac109d701541d55d7f1619d723a6
Brandon Schmidt <brandon@loopreturns.com>
2020-08-02 17:00:00 -0400

    Removed some copied content

--- server/app/controllers/api/v1/user_profile_videos_controller.rb
+++ server/app/controllers/api/v1/user_profile_videos_controller.rb
@@ -34,26 +34,6 @@ module Api
         end
       end
 
-    #   def update
-    #     result = ProfileService.update_user_profile(params[:id], user_profile_params)
-
-    #     if result.success?
-    #       render json: { errors: [], resource: profile_with_avatar(result.user_profile) }, status: :ok
-    #     else
-    #       render json: { errors: Array.wrap(result.user_profile&.errors&.full_messages), resource: nil }, status: :unprocessable_entity
-    #     end
-    #   end
-
-    #   def destroy
-    #     user_profile = UserProfile.find_by(user_id: params[:id])
-    #     if user_profile
-    #       user_profile.destroy
-    #       render json: { resource: nil }
-    #     else
-    #       render json: { errors: ["404 not found"], resource: nil }, status: :not_found
-    #     end
-    #   end
-
       def set_profile
         if params[:user_profile_id]
           @profile = UserProfile.find(params[:user_profile_id]) if params[:user_profile_id]
@@ -68,13 +48,6 @@ module Api
         user_profile = UserProfile.find_by(id: params[:user_profile_id], user_id: current_user.id)
         render json: {}, status: :unauthorized unless user_profile
       end
-
-    #   def profile_with_avatar(user_profile)
-    #     if user_profile&.avatar&.attached?
-    #       return user_profile.as_json.merge({ avatar: url_for(user_profile.avatar) })
-    #     end
-    #     user_profile
-    #   end
 
       def user_profile_video_params
         params.permit(:user_profile_video, :user_profile_id)
